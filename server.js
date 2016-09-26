13f260d3a2528aaecee6f031960f6b5f562ccdb7
nicholasxjy <yitaqi2011@gmail.com>
2016-09-26 16:11:34 +0800

    fix api data error

--- server.js
+++ server.js
@@ -4,17 +4,28 @@ var config = require('./webpack.config');
 
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  quiet: false,
-  stats: {
-    colors: true
-  }
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    quiet: false,
+    stats: {
+        colors: true
+    },
+    proxy: {
+        '/api/**': {
+            target: 'http://api.wallstreetcn.com/v2',
+            // ignorePath: true,
+            pathRewrite: {
+                '^/api': ''
+            },
+            changeOrigin: true,
+            logLevel: 'debug'
+        }
+    }
 }).listen(8080, 'localhost', function(err, result) {
-  if (err) {
-    console.log(err);
-    return false;
-  }
-  console.log('Listening at http://localhost:8080');
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    console.log('Listening at http://localhost:8080');
 });
